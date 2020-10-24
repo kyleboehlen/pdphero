a2a45a3bd7d39caacf9b5af5e681368d69fc86c2
kyleboehlen <kyleboehlen@gmail.com>
2020-10-24 16:21:36 -0600

    Starting blade templates

---
+++ resources/views/layouts/base.blade.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- CSRF Token -->
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>{{ config('app.name', 'PDPHero') }}</title>
+
+        <!-- Scripts -->
+        <script src="{{ asset('js/app.js') }}" defer></script>
+
+        <!-- Styles -->
+        <link href="@isset($stylesheet) {{ asset("css/$stylesheet.css") }} @else {{ asset('css/app.css') }} @endisset" rel="stylesheet">
+
+        <!-- Icons -->
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+        <link rel="manifest" href="/site.webmanifest">
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#155466">
+        <meta name="apple-mobile-web-app-title" content="PDPHero">
+        <meta name="application-name" content="PDPHero">
+        <meta name="msapplication-TileColor" content="#26130c">
+        <meta name="theme-color" content="#26130c">
+
+        {{-- HTML5 Shiv --}}
+        <!--[if lt IE 9]>
+            <script src="bower_components/html5shiv/dist/html5shiv.js"></script>
+        <![endif]-->
+    </head>
+    @yield('body')
+</html>
