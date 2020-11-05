456f85cdd69ecb14f094b5747dc0b03404821348
kyleboehlen <kyleboehlen@gmail.com>
2020-11-04 18:27:19 -0700

    PDP-41 Redo about page styles

--- resources/views/components/about-header.blade.php
+++ resources/views/components/about-header.blade.php
@@ -1,7 +1,8 @@
 <header class="about">
     <img id="hamburger-nav" class="hamburger" src="{{ asset('icons/hamburger-white.png') }}" />
+
     <h1 id="title">
-        <span class="pdp">PDP</span>&nbsp;&nbsp;<span class="hero">HERO</span>
+        <span class="pdp">PDP</span>&nbsp;<span class="hero">HERO</span>
     </h1>
     
     <a href="{{ route('login') }}"> 
--- resources/views/components/social-footer.blade.php
+++ resources/views/components/social-footer.blade.php
@@ -1,9 +1,7 @@
 <footer class="social">
-    <div class="links-container">
-        @foreach(config('socials') as $social)
-            <a href="{{ \buildSocialUrl($social) }}">
-                <img src="{{ asset('icons/' . $social['icon_name'] . '-white' . $social['icon_type']) }}" />
-            </a>
-        @endforeach
-    </div>
+    @foreach(config('socials') as $social)
+        <a href="{{ \buildSocialUrl($social) }}">
+            <img class="hover-white" src="{{ asset('icons/' . $social['icon_name'] . '-white' . $social['icon_type']) }}" />
+        </a>
+    @endforeach
 </footer>
