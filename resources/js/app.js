f78e20e981a65f5132f318f5e9456938639a18fb
kyleboehlen <kyleboehlen@gmail.com>
2020-11-24 16:15:30 -0700

    Fix jquery selector for toggling priority checkboxes

--- resources/js/app.js
+++ resources/js/app.js
@@ -25,16 +25,15 @@ $('.priority-checkbox').change(function() {
     var $box = $(this);
     if($box.is(":checked"))
     {
-      // the name of the box is retrieved using the .attr() method
-      // as it is assumed and expected to be immutable
-      var group = "input:checkbox[name='" + $box.attr("name") + "']";
-      // the checked state of the group/box on the other hand will change
-      // and the current value is retrieved using .prop() method
-      $(group).prop("checked", false);
-      $box.prop("checked", true);
+        // get all of the other checkboxes
+        var group = $('.priority-checkbox');
+        // the checked state of the group/box on the other hand will change
+        // and the current value is retrieved using .prop() method
+        $(group).prop("checked", false);
+        $box.prop("checked", true);
     }
     else
     {
-      $box.prop("checked", false);
+        $box.prop("checked", false);
     }
 });
