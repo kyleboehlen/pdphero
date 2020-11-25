2debf3c7ec4fe9131cc7e773559e8e7c284c8957
kyleboehlen <kyleboehlen@gmail.com>
2020-11-25 13:14:23 -0700

    Abstracted out the pop up box as a component

---
+++ resources/views/components/app/pop-up-box.blade.php
@@ -0,0 +1,5 @@
+<div class="{{ strtolower($title) }}-box">
+    <h1>{{ $title }}</h1>
+    <p>{{ $message }}</p><br/>
+    <button class="close-box" type="button">Okay</button><br/><br/>
+</div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,38 +5,22 @@
 
         {{-- Info box --}}
         @error('info')
-            <div class="info-box">
-                <h1>Info</h1>
-                <p>{{ $message }}</p>
-                <button class="close-box" type="button">Okay</button><br/><br/>
-            </div>
+            <x-app.pop-up-box title="Info" :message="$message" />
         @enderror
 
         {{-- Green success box --}}
         @error('success')
-            <div class="success-box">
-                <h1>Success</h1>
-                <p>{{ $message }}</p>
-                <button class="close-box" type="button">Okay</button><br/><br/>
-            </div>
+            <x-app.pop-up-box title="Success" :message="$message" />
         @enderror
 
         {{-- Yellow warning box --}}
         @error('warning')
-            <div class="warning-box">
-                <h1>Warning</h1>
-                <p>{{ $message }}</p>
-                <button class="close-box" type="button">Okay</button><br/><br/>
-            </div>
+            <x-app.pop-up-box title="Warning" :message="$message" />
         @enderror
 
         {{-- Red error box --}}
         @error('error')
-            <div class="error-box">
-                <h1>Error</h1>
-                <p>{{ $message }}</p>
-                <button class="close-box" type="button">Okay</button><br/><br/>
-            </div>
+            <x-app.pop-up-box title="Error" :message="$message" />
         @enderror
 
         @yield('template')
