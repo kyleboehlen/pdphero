65cc1f79f117510e638369be561cfad85f4f4864
kyleboehlen <kyleboehlen@gmail.com>
2021-03-06 18:46:18 -0700

    Empty goal component

--- resources/views/components/goals/empty-goal.blade.php
+++ resources/views/components/goals/empty-goal.blade.php
@@ -1,3 +1,18 @@
-<div>
-    <!-- The whole future lies in uncertainty: live immediately. - Seneca -->
+<div class="goal empty">
+    {{-- Goal image --}}
+    <img class="goal-img" src="{{ asset('icons/goal-status-tbd-black.png') }}" />
+
+    {{-- Goal title/link --}}
+    <a href="{{ route('goals.create.goal') }}">
+        <h2>Create Your First Goal</h2>
+    </a>
+
+    {{-- Goal progress bar --}}
+    <x-app.progress-bar percent="0" /><br/>
+
+    {{-- Status label/bar --}}
+    <div class="status" title="This goal needs to be created!">
+        <p class="label tbd">To Be Determined</p>
+        <img src="{{ asset('icons/goal-status-bar-tbd.png') }}" />
+    </div>
 </div>
