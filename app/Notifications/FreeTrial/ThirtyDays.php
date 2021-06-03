3edef81af77f00f3a1e584094eba36075d29baa1
kyleboehlen <kyleboehlen@gmail.com>
2021-06-02 21:12:47 -0600

    Add additional instructions to free trial notifications

--- app/Notifications/FreeTrial/ThirtyDays.php
+++ app/Notifications/FreeTrial/ThirtyDays.php
@@ -46,6 +46,7 @@ class ThirtyDays extends Notification
                     ->line('Looks like you have 30 days left in your free trial of PDPHero. Hopefully it has been helping you get on track to achieving your wildest dreams and ambitions!')
                     ->line('If there is anything you need help with, or anything we can do to make your experience better don\'t hesitate to reach out to us at support@pdphero.com or submit a support request within the app.')
                     ->line('If you\'ve decided PDPHero brings you value and you\'d like to continue using it, go ahead and subscribe so your service isn\'t interrupted. Don\'t worry, you\'ll still keep the 30 days left in your trial and won\'t be billed until it\'s over :)')
+                    ->line('You can subscribe by clicking the link, or go into your profile and click \'Manage Membership\'')
                     ->action('Subscribe', route('stripe'))
                     ->line('Thank you for using our application, and have a wonderful day!');
     }
--- app/Notifications/FreeTrial/ThreeDays.php
+++ app/Notifications/FreeTrial/ThreeDays.php
@@ -46,6 +46,7 @@ class ThreeDays extends Notification
                 ->line('Looks like you have 3 days left in your free trial of PDPHero. Don\'t worry, if you can\'t subscibe just yet, or you aren\'t ready, we\'ll keep all your goals and habits safe for you :)')
                 ->line('If there is anything you need help with, or if you have any feedback for us, don\'t hesitate to reach out to us at support@pdphero.com or submit a support request within the app.')
                 ->line('If you\'ve decided PDPHero brings you value and you\'d like to continue using it, go ahead and subscribe so your service isn\'t interrupted.')
+                ->line('You can subscribe by clicking the link, or go into your profile and click \'Manage Membership\'')
                 ->action('Subscribe', route('stripe'))
                 ->line('Thank you for trying PDPHero, and have a wonderful day!');
     }
