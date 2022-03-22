77bc88d81777d825eaf01b09d17f19fa212f24f5
kyleboehlen <kyleboehlen@gmail.com>
2022-03-22 15:05:45 -0600

    #24bz2kg change app env to use staging instead of testing

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\App;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,7 +25,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if($this->app->environment('production') || $this->app->environment('testing')) {
+        // if($this->app->environment('production') || $this->app->environment('staging')) {
+        if(App::enviroment(['production', 'staging'])) {
             \URL::forceScheme('https');
         }
     }
